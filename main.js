6d7f5f89a4c9b58d773fd412c818f6afa5eec88d
techang <>
2023-03-10 17:35:11 +0900

    change: 要約をさせる

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 var Imap = require("node-imap"),
   inspect = require("util").inspect;
 const { Configuration, OpenAIApi } = require("openai");
+const simpleParser = require('mailparser').simpleParser;
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 var imap = new Imap({
@@ -23,64 +26,52 @@ function openInbox(cb) {
 imap.once("ready", function () {
   openInbox(function (err, box) {
     if (err) throw err;
-    var f = imap.seq.fetch(box.messages.total + ":*", {
+    var f = imap.seq.fetch((box.messages.total) + ":*", {
       bodies: ["HEADER.FIELDS (FROM)", "TEXT"],
     });
     f.on("message", function (msg, seqno) {
-      console.log("Message #%d", seqno);
-      var prefix = "(#" + seqno + ") ";
+      console.log("Message #%d を処理しています。", seqno);
+      var prefix = '(#' + seqno + ') ';
+
       msg.on("body", function (stream, info) {
-        if (info.which === "TEXT")
-          console.log(
-            prefix + "Body [%s] found, %d total bytes",
-            inspect(info.which),
-            info.size
-          );
         var buffer = "",
           count = 0;
 
         stream.on("data", function (chunk) {
           count += chunk.length;
           buffer += chunk.toString("utf8");
-          if (info.which === "TEXT")
-            console.log(
-              prefix + "Body [%s] (%d/%d)",
-              inspect(info.which),
-              count,
-              info.size
-            );
         });
+
         stream.once("end", async function () {
-          if (info.which !== "TEXT")
-            console.log(
-              prefix + "Parsed header: %s",
-              inspect(Imap.parseHeader(buffer))
-            );
-          else console.log(prefix + "Body [%s] Finished", inspect(info.which));
+            // console.log(info.which, buffer
+                
+            //     )
+          if(info.which === "TEXT"){
+            let data = await simpleParser(buffer);
 
-          console.log(Buffer.from(buffer, "base64").toString());
+            completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                {
+                    role: "system",
+                    content:
+                    "受け取ったメールの文章を、箇条書きにして簡単に要約してください。提出物や返信が必要そうなものについては、強調して記述してください。",
+                },
+                {
+                    role: "user",
+                    content: data.text,
+                },
+                ],
+            });
+            console.log(completion.data.choices[0].message.content);
 
-          completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-              {
-                role: "system",
-                content:
-                  "受け取ったメールの文章を、箇条書きにして簡単に要約してください。提出物や返信が必要そうなものについては、強調して記述してください。",
-              },
-              {
-                role: "user",
-                content: Buffer.from(buffer, "base64").toString(),
-              },
-            ],
-          });
-          console.log(completion.data.choices);
+            }else{
+                console.log('Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+            }
         });
-      });
-      msg.once("attributes", function (attrs) {
-        console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
       });
       msg.once("end", function () {
+        
         console.log(prefix + "Finished");
       });
     });
